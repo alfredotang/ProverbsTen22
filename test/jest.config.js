1d9656936edb2dc0b26e294d08de190cdd59e7eb
alfredotang <asdzxcca0617@gmail.com>
2022-01-20 05:02:44 +0800

    chore: add nodemon & tsconfig-paths to log func

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   rootDir: '../',
-  moduleFileExtensions: ['ts', 'js', 'json'],
+  moduleFileExtensions: ['ts'],
   transform: {
     '^.+\\.(ts|js)$': 'babel-jest',
   },
   testMatch: ['<rootDir>/**/?(*.)spec.ts'],
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/test'],
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/test', '<rootDir>/log'],
   moduleNameMapper: {
     '@src(.*)$': '<rootDir>/src/$1',
     '@test(.*)$': '<rootDir>/test/$1',
